51f38b8c77b0e8ea7fe6c60a0e124b540b3dbd8d
cat-tourism-hub <catanduanestourismhub@gmail.com>
2024-06-25 13:06:29 +0800

    v0.0.1

--- firebase_conf.py
+++ firebase_conf.py
@@ -5,22 +5,23 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 from google.cloud import storage
 
-service_account_info = {
-    "type": 'service_account',
-    "project_id": os.environ.get('PROJECT_ID'),
-    "private_key_id": os.environ.get('PROJECT_KEY_ID'),
-    "private_key": os.environ.get('PRIVATE_KEY'),
-    "client_email": os.environ.get('CLIENT_EMAIL'),
-    "client_id": os.environ.get('CLIENT_ID'),
-    "auth_uri": os.environ.get('AUTH_URI'),
-    "token_uri": os.environ.get('TOKEN_URI'),
-    "auth_provider_x509_cert_url": os.environ.get('AUTH_PROVIDER_X509_CERT_URL'),
-    "client_x509_cert_url": os.environ.get('CLIENT_X509_CERT_URL'),
-    "universe_domain": os.environ.get('UNIVERSE_DOMAIN'),
-}
+# service_account_info = {
+#     "type": 'service_account',
+#     "project_id": os.environ.get('PROJECT_ID'),
+#     "private_key_id": os.environ.get('PROJECT_KEY_ID'),
+#     "private_key": os.environ.get('PRIVATE_KEY'),
+#     "client_email": os.environ.get('CLIENT_EMAIL'),
+#     "client_id": os.environ.get('CLIENT_ID'),
+#     "auth_uri": os.environ.get('AUTH_URI'),
+#     "token_uri": os.environ.get('TOKEN_URI'),
+#     "auth_provider_x509_cert_url": os.environ.get('AUTH_PROVIDER_X509_CERT_URL'),
+#     "client_x509_cert_url": os.environ.get('CLIENT_X509_CERT_URL'),
+#     "universe_domain": os.environ.get('UNIVERSE_DOMAIN'),
+# }
 
 
-cred = credentials.Certificate(service_account_info)
+cred = credentials.Certificate('/etc/secrets/fb_key')
 firebase_admin.initialize_app(cred)
 admin_firestore = firestore.client()
-storage_client = storage.Client.from_service_account_json(service_account_info)
+storage_client = storage.Client.from_service_account_json(
+    '/etc/secrets/fb_key')
