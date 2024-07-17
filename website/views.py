3dc9bb0319368da180e32a7fac9b54ca4ff4961e
cat-tourism-hub <catanduanestourismhub@gmail.com>
2024-07-17 12:40:44 +0800

    v0.0.2

--- website/views.py
+++ website/views.py
@@ -3,19 +3,21 @@ from flask import Blueprint, redirect, jsonify
 from firebase_conf import admin_firestore as db
 from .strings import *
 from google.cloud.firestore_v1.base_query import FieldFilter
+from google.api_core.exceptions import DeadlineExceeded
 
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
+@views.route('/', methods=['GET'])
 def index():
     try:
         partners_ref = db.collection(
             PARTNERS).where(filter=FieldFilter("estb.status", "==", "Pending"))
-        partners = partners_ref.get()
+        partners = partners_ref.get(timeout=10)
         partners_list = [partner.to_dict() for partner in partners]
 
         return partners_list
-
+    except DeadlineExceeded:
+        return jsonify({"Error": "Network timed out. Please check internet connection."}), 504
     except Exception as e:
         return jsonify({'error': str(e)}), 500
