4ee22e751b9864ee2557c2ff33f1f8dfe00dcde5
cat-tourism-hub <catanduanestourismhub@gmail.com>
2024-08-05 20:37:21 +0800

    v0.0.2

--- firebase_conf.py
+++ firebase_conf.py
@@ -8,8 +8,8 @@ from google.cloud import storage
 # Use /etc/secrets/<filename> on server
 # Use ./tourism-hub.json on local
 
-cred = credentials.Certificate('/etc/secrets/<filename>')
+cred = credentials.Certificate('/etc/secrets/fb_key')
 firebase_admin.initialize_app(cred)
 admin_firestore = firestore.client()
 storage_client = storage.Client.from_service_account_json(
-    '/etc/secrets/<filename>')
+    '/etc/secrets/fb_key')
